d820c4ecb3c6a14fb6811dcc0280b8222befd6b9
antongoncharik <ant.goncharik@gmail.com>
2024-04-17 19:22:18 +0300

    Test

--- internal/api/grpc/package.go
+++ internal/api/grpc/package.go
@@ -17,16 +17,18 @@ var gRPCClients *GRPCClients
 var clientConn *grpc.ClientConn
 
 func Connect() {
+	fmt.Println("HELLO 0")
 	conn, err := grpc.Dial("113.30.189.245:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
+	fmt.Println("HELLO 1")
 
 	clientConn = conn
 	gRPCClients = &GRPCClients{Common: pbCommon.NewCommonServiceClient(clientConn)}
-	fmt.Println("HELLO")
-	Get().Common.SwitchOn(context.Background(), &pbCommon.EmptyRequest{})
 	fmt.Println("HELLO 2")
+	Get().Common.SwitchOn(context.Background(), &pbCommon.EmptyRequest{})
+	fmt.Println("HELLO 3")
 }
 
 func Get() *GRPCClients {
