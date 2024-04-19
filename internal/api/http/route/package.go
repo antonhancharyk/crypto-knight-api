9d491ab0c135faa77be9457d2b9a4a1e3e59043e
antongoncharik <ant.goncharik@gmail.com>
2024-04-19 22:29:10 +0300

    Add cors

--- internal/api/http/route/package.go
+++ internal/api/http/route/package.go
@@ -2,12 +2,15 @@ package route
 
 import (
 	"github.com/antongoncharik/crypto-knight-api/internal/api/http/handler"
+	"github.com/antongoncharik/crypto-knight-api/internal/api/http/middleware/cors"
 	"github.com/antongoncharik/crypto-knight-api/internal/api/http/route/common"
 	"github.com/gin-gonic/gin"
 )
 
 func Init(hdl *handler.Handler) *gin.Engine {
 	router := gin.Default()
+
+	router.Use(cors.UseCORS())
 
 	common.UseCommonRoutes(router, hdl)
 
