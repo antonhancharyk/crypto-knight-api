8b46f9800d8d585aae108adc4150a6ad890a0759
antongoncharik <ant.goncharik@gmail.com>
2024-05-21 23:07:29 +0300

    Remove validation from track

--- internal/entity/track/track.go
+++ internal/entity/track/track.go
@@ -4,8 +4,8 @@ import "time"
 
 type Track struct {
 	Symbol    string    `json:"symbol" db:"symbol" validate:"required"`
-	HighPrice float64   `json:"high_price" db:"high_price" validate:"required,numeric"`
-	LowPrice  float64   `json:"low_price" db:"low_price" validate:"required,numeric"`
+	HighPrice float64   `json:"high_price" db:"high_price" validate:"numeric"`
+	LowPrice  float64   `json:"low_price" db:"low_price" validate:"numeric"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
