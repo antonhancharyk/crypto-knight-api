6f35cf4e108ae230b4c73ecdb662767d27f2313d
antongoncharik <ant.goncharik@gmail.com>
2025-01-04 00:58:46 +0300

    Change cerbot

--- config/certbot/start_certbot.sh
+++ config/certbot/start_certbot.sh
@@ -15,7 +15,7 @@ while :; do
   else
       echo "Certificate found. Checking expiry date."
       expiry_date=$(openssl x509 -enddate -noout -in $cert_path | cut -d= -f2)
-      expiry_seconds=$(date -u -d "$(echo $expiry_date)" +%s)
+      expiry_seconds=$(date -d "$expiry_date" +%s)
       current_seconds=$(date +%s)
       diff_seconds=$((expiry_seconds - current_seconds))
 
