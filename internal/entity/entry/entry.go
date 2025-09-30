ea027c6f25409d3a3dfe99d3aa33d5c53c092472
antongoncharik <ant.goncharik@gmail.com>
2025-09-30 15:42:45 +0300

    Change array type

--- internal/entity/entry/entry.go
+++ internal/entity/entry/entry.go
@@ -2,16 +2,18 @@ package entry
 
 import (
 	"time"
+
+	"github.com/lib/pq"
 )
 
 type Entry struct {
-	Symbol     string    `json:"symbol" db:"symbol" validate:"required"`
-	HighPrice  float64   `json:"high_price" db:"high_price" validate:"numeric"`
-	LowPrice   float64   `json:"low_price" db:"low_price" validate:"numeric"`
-	HighPrices []float64 `json:"high_prices" db:"high_prices"`
-	LowPrices  []float64 `json:"low_prices" db:"low_prices"`
-	CreatedAt  time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at" db:"updated_at"`
+	Symbol     string          `json:"symbol" db:"symbol" validate:"required"`
+	HighPrice  float64         `json:"high_price" db:"high_price" validate:"numeric"`
+	LowPrice   float64         `json:"low_price" db:"low_price" validate:"numeric"`
+	HighPrices pq.Float64Array `json:"high_prices" db:"high_prices"`
+	LowPrices  pq.Float64Array `json:"low_prices" db:"low_prices"`
+	CreatedAt  time.Time       `json:"created_at" db:"created_at"`
+	UpdatedAt  time.Time       `json:"updated_at" db:"updated_at"`
 }
 
 type QueryParams struct {
