8c82c92959633929dc3c42ed3e758dc62f6763f9
antongoncharik <ant.goncharik@gmail.com>
2025-12-20 23:33:36 +0300

    Fix middle price

--- migrations/0016_add_middle_price.up.sql
+++ migrations/0016_add_middle_price.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE tracks_history ADD COLUMN middle_price_high NUMERIC NOT NULL;
-ALTER TABLE tracks_history ADD COLUMN middle_price_low NUMERIC NOT NULL;
+ALTER TABLE tracks_history ADD COLUMN middle_price_high NUMERIC NOT NULL DEFAULT 0;
+ALTER TABLE tracks_history ADD COLUMN middle_price_low NUMERIC NOT NULL DEFAULT 0;
